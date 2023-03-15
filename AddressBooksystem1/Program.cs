213feee2bd8823e3e9e9faaea3cf1f9fa85967b7
unknown <amoljadhav>
2023-03-15 21:15:01 +0530

    [Amol]Add:Contacts

--- AddressBooksystem1/Program.cs
+++ AddressBooksystem1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace AddressBooksystem1
+﻿using AddressBook;
+
+namespace AddressBooksystem1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To The Address Book System!");
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter ZIP");
+            contact.ZIP = Console.ReadLine();
+            Console.WriteLine("Enter Phonenumber");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            contact.Email = Console.ReadLine();
+           
         }
     }
 }
