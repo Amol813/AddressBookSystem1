a2a8a7fa031c5e0fae386d5fa0f8f0e878df87da
unknown <amoljadhav>
2023-03-15 23:21:02 +0530

    [Amol]Add:AddressBookMain

---
+++ AddressBooksystem1/AddressBookMain.cs
@@ -0,0 +1,42 @@
+﻿using AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooksystem1
+{
+    public class AddressBookMain
+    {
+        Contact contact = new Contact();
+        public void AddContact()
+        {
+            Console.WriteLine("Welcome To The Address Book System!");
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter ZIP");
+            contact.ZIP = Console.ReadLine();
+            Console.WriteLine("Enter Phonenumber");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            contact.Email = Console.ReadLine();
+
+        }
+        public void Display()
+        {
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.ZIP + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
+    
+    
+    }
+}
--- AddressBooksystem1/Program.cs
+++ AddressBooksystem1/Program.cs
@@ -6,25 +6,10 @@ namespace AddressBooksystem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The Address Book System!");
-            Contact contact = new Contact();
-            Console.WriteLine("Enter First Name");
-            contact.FirstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            contact.LastName = Console.ReadLine();
-            Console.WriteLine("Enter Address");
-            contact.Address = Console.ReadLine();
-            Console.WriteLine("Enter City");
-            contact.City = Console.ReadLine();
-            Console.WriteLine("Enter State");
-            contact.State = Console.ReadLine();
-            Console.WriteLine("Enter ZIP");
-            contact.ZIP = Console.ReadLine();
-            Console.WriteLine("Enter Phonenumber");
-            contact.PhoneNumber = Console.ReadLine();
-            Console.WriteLine("Enter Email");
-            contact.Email = Console.ReadLine();
-           
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.AddContact();
+            addressBookMain.Display();
         }
+       
     }
 }
